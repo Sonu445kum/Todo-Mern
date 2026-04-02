4f42807b5d5b5778b77538bcd2fab16869122c24
Sonu445Kum <sonuroy1629@gmail.com>
2026-04-02 13:14:02 +0530

    Create User Models

---
+++ backend/models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name:String,
+    email :{
+        type:String,
+        unique:true,
+        require:true
+    },
+    password :String,
+},{timestamps:true});
+
+// export here
+module.exports = mongoose.model("user",userSchema);
